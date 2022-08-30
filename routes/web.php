745333bb6e15243ec152d4cecc44278eba436010
Ariful Alam <ariful.alam@monstar-lab.com>
2022-08-30 22:26:38 +0600

    View local file logs

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,11 @@
 <?php
 
+use App\Http\Controllers\PandoraController;
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/', [PandoraController::class, 'index']);
 
-Route::get('/', function () {
-    return view('welcome');
-});
+if (Config::get('app.debug')) {
+    Route::get('/logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
+}
