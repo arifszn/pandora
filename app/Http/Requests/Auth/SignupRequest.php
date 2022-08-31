fb09cd734b08fa6fab46b7b9fb30ef4cc5dbcb35
Ariful Alam <ariful.alam@monstar-lab.com>
2022-08-31 17:18:57 +0600

    Add SignupRequest schema

--- app/Http/Requests/Auth/SignupRequest.php
+++ app/Http/Requests/Auth/SignupRequest.php
@@ -3,7 +3,35 @@
 namespace App\Http\Requests\Auth;
 
 use Illuminate\Foundation\Http\FormRequest;
+use OpenApi\Attributes as OAT;
 
+#[OAT\Schema(
+    schema: 'SignupRequest',
+    required: ['name', 'email', 'password', 'password_confirmation'],
+    properties: [
+        new OAT\Property(
+            property: 'name',
+            type: 'string',
+            example: 'John Doe'
+        ),
+        new OAT\Property(
+            property: 'email',
+            type: 'string',
+            format: 'email',
+            example: 'john@example.com'
+        ),
+        new OAT\Property(
+            property: 'password',
+            type: 'string',
+            example: '123456'
+        ),
+        new OAT\Property(
+            property: 'password_confirmation',
+            type: 'string',
+            example: '123456'
+        ),
+    ]
+)]
 class SignupRequest extends FormRequest
 {
     /**
