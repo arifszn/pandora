7b5da574158a2908f30dd53346a4333785210b9b
Ariful Alam <ariful.alam@monstar-lab.com>
2022-08-31 15:00:26 +0600

    Create AuthService

---
+++ app/Services/AuthService.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Services;
+
+use App\Http\Requests\Auth\SignupRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AuthService
+{
+    /**
+     * Create a new service instance.
+     *
+     * @param  UserService  $userService
+     * @return void
+     */
+    public function __construct(private UserService $userService)
+    {
+        //
+    }
+
+    /**
+     * Signup a user.
+     *
+     * @param SignupRequest $request 
+     * @return User 
+     */
+    public function signupUser(SignupRequest $request): User
+    {
+        $user = $this->userService->storeUser([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return $user;
+    }
+}
