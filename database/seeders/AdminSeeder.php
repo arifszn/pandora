170c957a37fc10419e0abb5e5e307cebd4579940
Ariful Alam <ariful.alam@monstar-lab.com>
2022-09-03 22:13:16 +0600

    Create seeder for admin

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Admin;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Admin::create([
+            'name' => 'Admin',
+            'email' => 'root@admin.com',
+            'password' => Hash::make('123456'),
+        ]);
+    }
+}
