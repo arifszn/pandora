dd8b1dd8196443ab4fa96d365785df413a948ead
Ariful Alam <ariful.alam@monstar-lab.com>
2022-09-03 17:31:57 +0600

    Create event UserSignedUp

---
+++ app/Events/UserSignedUp.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class UserSignedUp
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+}
