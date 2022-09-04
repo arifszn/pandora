c89a10f14bde508e8d5330caa5dcfc1b59dd3056
Ariful Alam <ariful.alam@monstar-lab.com>
2022-09-04 17:50:13 +0600

    Pass user object in constructor

--- app/Events/UserSignedUp.php
+++ app/Events/UserSignedUp.php
@@ -3,6 +3,7 @@
 namespace App\Events;
 
 use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
@@ -12,10 +13,11 @@ class UserSignedUp
 
     /**
      * Create a new event instance.
-     *
-     * @return void
+     * 
+     * @param Authenticatable $user 
+     * @return void 
      */
-    public function __construct()
+    public function __construct(public Authenticatable $user)
     {
         //
     }
