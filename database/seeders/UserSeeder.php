f8cd17cc6f527ca20e3c5ad123f46ef5352ecb7f
Ariful Alam <ariful.alam@monstar-lab.com>
2022-09-16 15:08:26 +0600

    Create seeder for user

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Admin;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Admin::create([
+            'name' => 'User',
+            'email' => 'john@example.com',
+            'password' => Hash::make('123456'),
+        ]);
+    }
+}
