571821728610f2d1ecb483cd12642014bec379d3
Ariful Alam <swazan.arif@gmail.com>
2023-01-28 01:58:25 +0600

    Display admin view

---
+++ resources/views/admin/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Favicon -->
+        <link rel="shortcut icon" type="image/x-icon" href="{{asset('favicon.ico')}}">
+        <title>{{Config::get('app.name')}}</title>
+        
+        {{-- @viteReactRefresh
+        @vite('resources/js/app.jsx') --}}
+    </head>
+    <body id="body">
+        <div id="react-root"></div>
+        HELLO WORLD
+    </body>
+</html>
