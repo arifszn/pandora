23ace2910e830be7de5dd811bf8d2bcec8a59f49
Ariful Alam <swazan.arif@gmail.com>
2023-01-28 17:57:30 +0600

    Setup eslint and prettier

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,8 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-        <div>ADMIN APP</div>
-    </React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <div>ADMIN APP</div>
+  </React.StrictMode>
 );
